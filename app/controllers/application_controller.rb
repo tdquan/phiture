ed005009ffefb1f22480fffb4ba61cfb8c0630a9
Quan Trinh <trinhquan1123@yahoo.com>
2020-03-24 13:14:30 +0800

    [config] devise routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,39 @@
+# frozen_string_literal: true
+
 class ApplicationController < ActionController::API
+  def render_resource(resource)
+    if resource.errors.empty?
+      render json: resource
+    else
+      resource_error(resource, 'validation')
+    end
+  end
+
+  def resource_error(resource, type)
+    case type
+    when 'not_found'
+      render json: error_hash(
+        '404', 'Not Found', resource.errors, '100'
+      ), status: :not_found
+    else
+      render json: error_hash(
+        '400', 'Bad Request', resource.errors, '100'
+      ), status: :bad_request
+    end
+  end
+
+  private
+
+  def error_hash(status, title, details, code)
+    {
+      errors: [
+        {
+          status: status,
+          title: title,
+          details: details,
+          code: code
+        }
+      ]
+    }
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable
-         # :jwt_authenticatable,
-         # jwt_revocation_strategy: JWTBlacklist
+  devise :database_authenticatable,
+         :jwt_authenticatable,
+         jwt_revocation_strategy: JwtBlacklist
 end
